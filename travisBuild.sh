bfa2215db8fcd4b9d277c83af3fc30d92f05c076
Igor Bolic <igor.bolic@sixhours.io>
2017-08-15 16:16:51 +0200

    Move travis script in separate file

---
+++ travisBuild.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+  echo -e "Build Pull Request #$TRAVIS_PULL_REQUEST => Branch [$TRAVIS_BRANCH]"
+  ./gradlew build
+elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_TAG" == "" ]; then
+  echo -e 'Build Branch with Snapshot => Branch ['$TRAVIS_BRANCH']'
+  ./gradlew build
+elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_TAG" != "" ]; then
+  echo -e 'Build Branch for Release => Branch ['$TRAVIS_BRANCH']  Tag ['$TRAVIS_TAG']'
+  case "$TRAVIS_TAG" in
+  v[[:digit:]]*\.[[:digit:]]*\.[[:digit:]]*)
+    ./gradlew build bintrayUpload
+    ;;
+  *)
+    echo -e 'WARN: Invalid Tag ['$TRAVIS_TAG']'
+    ;;
+  esac
+else
+  echo -e 'WARN: Should not be here => Branch ['$TRAVIS_BRANCH']  Tag ['$TRAVIS_TAG']  Pull Request ['$TRAVIS_PULL_REQUEST']'
+  ./gradlew build
+fi
