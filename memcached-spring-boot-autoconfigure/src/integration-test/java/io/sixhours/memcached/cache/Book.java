4e53fae6fd54d59a7d9a17de7b50fcd447fbd4b9
Igor Bolic <igor.bolic@sixhours.io>
2017-11-03 11:50:38 +0100

    Add integration tests

---
+++ memcached-spring-boot-autoconfigure/src/integration-test/java/io/sixhours/memcached/cache/Book.java
@@ -0,0 +1,44 @@
+package io.sixhours.memcached.cache;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Book implements Serializable {
+        private Integer id;
+        private String title;
+        private Integer year;
+
+        public Book(Integer id, String title, Integer year) {
+            this.id = id;
+            this.title = title;
+            this.year = year;
+        }
+
+        public Integer getId() {
+            return id;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public Integer getYear() {
+            return year;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Book book = (Book) o;
+            return Objects.equals(id, book.id) &&
+                    Objects.equals(title, book.title) &&
+                    Objects.equals(year, book.year);
+        }
+
+        @Override
+        public int hashCode() {
+
+            return Objects.hash(id, title, year);
+        }
+    }
