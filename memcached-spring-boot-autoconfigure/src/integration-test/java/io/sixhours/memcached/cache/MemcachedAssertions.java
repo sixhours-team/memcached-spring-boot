ea654648bd14448c3b9c31bc663ee8a41521ba31
Igor Bolic <igor.bolic@sixhours.io>
2020-01-22 12:28:19 +0100

    Assert memcached server list only if existing

--- memcached-spring-boot-autoconfigure/src/integration-test/java/io/sixhours/memcached/cache/MemcachedAssertions.java
+++ memcached-spring-boot-autoconfigure/src/integration-test/java/io/sixhours/memcached/cache/MemcachedAssertions.java
@@ -79,11 +79,14 @@ public final class MemcachedAssertions {
 
         assertThat(nativeCache.getOpTimeout()).isEqualTo(operationTimeout);
         assertThat(connector.getProtocol().name().toUpperCase()).isEqualTo(protocol.name());
-        assertThat(availableServers)
-                .as("The number of memcached node endpoints should match server list size")
-                .hasSize(servers.length);
 
         final List<InetSocketAddress> actualServers = Arrays.asList(servers);
+
+        if (actualServers.size() > 0) {
+            assertThat(availableServers)
+                    .as("The number of memcached node endpoints should match server list size")
+                    .hasSize(servers.length);
+        }
 
         for (int i = 0; i < availableServers.length; i++) {
             InetSocketAddress address = availableServers[i];
